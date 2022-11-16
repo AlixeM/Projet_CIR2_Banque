08bdf650b5454852e1dcd793bceb11949d05a40d
Alixe <alixe.maerte@student.junia.com>
2022-11-16 21:45:44 +0100

    Widget it is

---
+++ client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrateur on 16/11/2022.
+//
+
+#include "client.h"
---
+++ client.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrateur on 16/11/2022.
+//
+
+#ifndef PATHYS_CLIENT_H
+#define PATHYS_CLIENT_H
+
+#endif //PATHYS_CLIENT_H
