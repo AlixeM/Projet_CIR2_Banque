a353b2256ae25c4c699922956527785e038e76d5
Alixe <alixe.maerte@student.junia.com>
2022-11-16 23:05:24 +0100

    BENJI LE BOSS

---
+++ cmake-build-debug/CMakeFiles/PATHYS.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PATHYS.dir/main.cpp.obj"
+  "CMakeFiles/PATHYS.dir/main.cpp.obj.d"
+  "PATHYS.exe"
+  "PATHYS.exe.manifest"
+  "PATHYS.lib"
+  "PATHYS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PATHYS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
