5e66e75154f16210a13b7ad7291fea8c894dedfb
Alixe <alixe.maerte@student.junia.com>
2022-11-22 15:25:12 +0100

    coucou

--- main.cpp
+++ main.cpp
@@ -8,8 +8,5 @@ bool MyApp::OnInit()
     Simple *simple = new Simple(wxT("PATHYS BANQUE"));
     simple->Show(true);
 
-    Icon *icon = new Icon(wxT("Icon"));
-    icon->Show(true);
-
     return true;
 }
