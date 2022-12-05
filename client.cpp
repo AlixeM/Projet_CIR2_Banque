05a00163558321853e886c98377de0f5e7b4347e
RaphSoko <raphael.sokolowski@student.junia.com>
2022-12-05 11:02:36 +0100

    classes et quelques fonctions

--- client.cpp
+++ client.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "client.h"
+
+creer_ptree_client(const Client& client) {
+	ptree pt;
+
+	pt.put("Numclient", client.numclient);
+	pt.put("Nom", client.nom);
+	pt.put("Prenom", client.prenom);
+
+	return pt;
+}
+
+Client extraire_client(ptree& pt) {
+	pt.get<int>("Numclient", 0);
+	std::string nom = pt.get("Nom");
+	std::string prenom = pt.get("Prenom");
+}
--- client.h
+++ client.h
@@ -1,8 +1,48 @@
 //
 // Created by Administrateur on 16/11/2022.
 //
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <fstream>
+
+using boost::property_tree::ptree;
+using boost::property_tree::read_json;
+using boost::property_tree::write_json;
+
 
 #ifndef PATHYS_CLIENT_H
 #define PATHYS_CLIENT_H
 
 #endif //PATHYS_CLIENT_H
+
+class Client {
+	int numclient;
+	std::string nom;
+	std::string prenom;
+	std::vector<int> num_compte;
+	Client();
+	Client(const int& numclient, std::string&& name, std::string&& prenom,std::vector<int> num_compte);
+};
+
+class Compte {
+	int numclient;
+	int numcompte;
+	int type;
+	int solde;
+	int agence;
+	Compte();
+	Compte(const int& numclient,const int& numcompte, const int& type, const int& solde, const int& agence);
+};
+
+ptree creer_ptree_client(const Client& client);
+
+ptree creer_ptree_compte(const Compte& compte);
+
+Client extraire_client(ptree& pt);
+
+Compte extraire_compte(ptree& pt);
