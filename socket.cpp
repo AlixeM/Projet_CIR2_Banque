5420ec8c11691f069620367224ecaff8d7e407cd
Mathys Mathon <mathys.mathon@student.junia.com>
2022-12-29 17:05:54 +0100

    uwu

--- socket.cpp
+++ socket.cpp
@@ -18,7 +18,6 @@
 #include <thread>
 #include <chrono>
 
-#include "client.hpp"
 #include "socket_tools.hpp"
 
 //Taken from "https://www.boost.org/doc/libs/1_80_0/doc/html/boost_asio/example/cpp11/echo/blocking_tcp_echo_server.cpp" with modifications
