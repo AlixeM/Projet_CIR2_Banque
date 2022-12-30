b792687cfe38d15de0f3ccff810cb7c094944021
Alexandre <alexandre.deberdt@student.junia.com>
2022-12-30 18:18:00 +0100

    uydufigk

--- essai.cpp
+++ essai.cpp
@@ -4,11 +4,13 @@ int main() {
 
 
 	cout << heure_depart;
-	cout << " seconds has passed since 00:00:00 GMT, Jan 1, 1970";
 
 	return 0;
 }
 
-//quand on change le solde sur le compte, on change la variable heure_depart à time(NULL)
+//quand on crée le compte, on change la variable heure_depart à time(NULL)
 //et quand on re-regarde le solde la fois d'après, on change le solde à solde+(solde*(solde*(time(NULL)-heure_depart)))
 //et on remet heure_depart à time(NULL)
+
+
+
